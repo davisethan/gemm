77eb797203dacbc8dc3c350a17aa459d95035423
Ethan Davis <ethandavisecd@gmail.com>
2025-06-25 12:41:11 -0700

    Updated execution experiment

--- execute.sh
+++ execute.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-sizes=(100 200 300 400 500 600 700 800 900
-    1000 2000 3000 4000 5000 6000 7000 8000 9000
-    10000)
-for size in "${sizes[@]}"; do
+sizes=(10 100 1000 2000 3000 4000 5000 6000 7000 8000 9000 10000)
+for size in "${sizes[@]}"
+do
     ./compile.sh $size
-    ./mainCuda
-    echo ""
-    ./main
-    echo ""
+    for it in {1..30}
+    do
+        echo "Iteration $it"
+        echo ""
+        ./mainCuda
+        echo ""
+        ./main
+        echo ""
+    done
 done
